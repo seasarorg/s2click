10e24e3c1ec75af96b073afda89e3a16a1460607
takezoe <takezoe@gmail.com>
2008-08-01 07:49:09 +0000

    アノテーションに@Documentedを追加。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2click/trunk/s2click@185 595bf13c-eed3-438f-ae63-4794fb747ba6

--- src/org/seasar/s2click/annotation/Request.java
+++ src/org/seasar/s2click/annotation/Request.java
@@ -1,5 +1,6 @@
 package org.seasar.s2click.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -12,6 +13,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Request {
 
 	/**
