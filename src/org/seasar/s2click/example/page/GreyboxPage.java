f12545bf70fb79c00851fb63e62f8728786f6718
takezoe <takezoe@gmail.com>
2009-01-08 17:48:41 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2click/trunk/s2click@315 595bf13c-eed3-438f-ae63-4794fb747ba6

--- src/org/seasar/s2click/example/page/GreyboxPage.java
+++ src/org/seasar/s2click/example/page/GreyboxPage.java
@@ -1,16 +1,32 @@
 package org.seasar.s2click.example.page;
 
+import org.seasar.s2click.annotation.Request;
+import org.seasar.s2click.control.GreyboxButton;
+import org.seasar.s2click.control.GreyboxLink;
 import org.seasar.s2click.example.form.GreyboxForm;
 
 /**
- *
+ * {@link GreyboxButton}と{@link GreyboxLink}のサンプルです。
+ * <p>
+ * Greyboxを使用して値を選択するためのモーダルダイアログを表示します。
+ * 
  * @author Naoki Takezoe
  * @since 0.4.0
  */
 public class GreyboxPage extends LayoutPage {
 
 	public String title = "GreyBox";
+	
+	public GreyboxLink link = new GreyboxLink("link", "参照...", "ユーザを選択", GreyboxSelectPage.class);
+	
+	@Request
+	public String userId;
 
 	public GreyboxForm form = new GreyboxForm("form");
-
+	
+	public void onInit(){
+		super.onInit();
+		form.userId.setValue(userId);
+	}
+	
 }
