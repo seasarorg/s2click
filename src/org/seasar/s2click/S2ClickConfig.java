87920d3449322cc721007469d184a2fdd92bc3b2
takezoe <takezoe@gmail.com>
2009-01-09 16:32:51 +0000

    [CLICK-29]S2ClickConfigのfileItemFactoryに@Binding(bindingType=BindingType.NONE)を付与しました。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2click/trunk/s2click@320 595bf13c-eed3-438f-ae63-4794fb747ba6

--- src/org/seasar/s2click/S2ClickConfig.java
+++ src/org/seasar/s2click/S2ClickConfig.java
@@ -10,25 +10,27 @@ import net.sf.click.util.Format;
 
 import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.seasar.framework.container.annotation.tiger.Binding;
+import org.seasar.framework.container.annotation.tiger.BindingType;
 
 /**
  * diconファイルでClickの設定を行うためのクラス。
- * 
+ *
  * @author Naoki Takezoe
  */
 public class S2ClickConfig {
-	
+
 	/**
 	 * 文字コード。デフォルトはUTF-8です。
 	 */
 	public String charset = "UTF-8";
-	
+
 	/**
 	 * ページテンプレートで利用可能なフォーマットクラス。
 	 * デフォルトは<code>net.sf.click.util.Format</code>です。
 	 */
 	public Class<? extends Format> formatClass = Format.class;
-	
+
 	/**
 	 * Clickの動作モード。以下の値が指定可能です。デフォルトはdevelopmentです。
 	 * <ul>
@@ -42,43 +44,44 @@ public class S2ClickConfig {
 	 * 詳細については<a href="http://click.sourceforge.net/docs/configuration.html#application-configuration">Clickのドキュメント</a>を参照してください。
 	 */
 	public String mode = "development";
-	
+
 	/**
 	 * 共通レスポンスヘッダ。
 	 */
 	public Map<String, String> headers = new HashMap<String, String>();
-	
+
 	/**
 	 * ロケール。
 	 */
 	public String locale;
-	
+
 //	/**
 //	 * ページクラスにリクエストパラメータを自動バインドするかどうか。
 //	 * デフォルトは<code>true</code>です。
 //	 */
 //	public boolean autoBinding = true;
-	
+
 	/**
 	 * コントロールセットを定義した設定ファイル群のパス。
 	 */
 	public List<String> controlSets = new ArrayList<String>();
-	
+
 	/**
 	 * デプロイ（ファイルの展開）が必要なコントロールクラス群。
 	 */
 	public List<Class<? extends Control>> controls = new ArrayList<Class<? extends Control>>();
-	
+
 	/**
 	 * Clickの<code>FileField</code>コントロールが使用するCommons FileUploadの<code>FileItemFactory</code>のインスタンス。
 	 * デフォルトは<code>org.apache.commons.fileupload.disk.DiskFileItemFactory</code>です。
 	 */
+	@Binding(bindingType = BindingType.NONE)
 	public FileItemFactory fileItemFactory = new DiskFileItemFactory();
-	
+
 	public S2ClickConfig(){
 		controlSets.add("org/seasar/s2click/s2click-controls.xml");
 //		controls.add(ToolTip.class);
 //		controls.add(CodePrettify.class);
 	}
-	
+
 }
