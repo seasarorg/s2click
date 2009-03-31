4530f9c8bbfb67757a2f77f613b975d4c8a23d45
takezoe <takezoe@gmail.com>
2009-03-31 15:30:51 +0000

    Javadocを記述。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2click/trunk/s2click@460 595bf13c-eed3-438f-ae63-4794fb747ba6

--- test/org/seasar/s2click/Assert.java
+++ test/org/seasar/s2click/Assert.java
@@ -7,7 +7,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 
+ * {@link S2ClickServiceTestCase}を継承したテストケースのテストメソッドに付与することで、
+ * 指定したテーブルの内容とExcelの内容を比較することができます。
+ * <p>
+ * また、テストケース実行時にExcelが存在しない場合は自動的に生成されます。
  * 
  * @author Naoki Takezoe
  */
@@ -16,11 +19,33 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Assert {
 	
+	/**
+	 * 比較対象のテーブル。
+	 */
 	Table[] tables();
 	
+	/**
+	 * {@link Assert}アノテーションで比較するテーブルの情報を指定するアノテーションです。
+	 * 
+	 * @author Naoki Takezoe
+	 */
 	public @interface Table {
+		
+		/**
+		 * テーブル名。
+		 */
 		String name();
+		
+		/**
+		 * 比較対象のカラム名。
+		 * この属性を省略し、なおかつ<code>excludeColumns</code>を指定しなかった場合はすべてのカラムが比較対象になります。
+		 */
 		String[] includeColumns() default {};
+		
+		/**
+		 * 比較対象外のカラム名。
+		 * この属性を省略し、なおかつ<code>includeColumns</code>を指定しなかった場合はすべてのカラムが比較対象になります。
+		 */
 		String[] excludeColumns() default {};
 	}
 	
