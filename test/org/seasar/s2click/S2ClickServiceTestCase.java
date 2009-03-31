659367099490dc8fadfd1cc1926847e0dfa6ccc5
takezoe <takezoe@gmail.com>
2009-03-31 05:39:18 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2click/trunk/s2click@456 595bf13c-eed3-438f-ae63-4794fb747ba6

--- test/org/seasar/s2click/S2ClickServiceTestCase.java
+++ test/org/seasar/s2click/S2ClickServiceTestCase.java
@@ -1,7 +1,21 @@
 package org.seasar.s2click;
 
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
+import javax.transaction.TransactionManager;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.seasar.extension.jdbc.JdbcContext;
+import org.seasar.extension.jdbc.JdbcManager;
+import org.seasar.extension.jdbc.manager.JdbcManagerImplementor;
 import org.seasar.framework.container.SingletonS2Container;
 import org.seasar.framework.util.tiger.GenericUtil;
 
@@ -20,6 +34,8 @@ public abstract class S2ClickServiceTestCase<T> extends S2ClickTestCase {
 	 */
 	protected T service;
 	
+	protected JdbcManager jdbcManager;
+	
 	@Override
 	@SuppressWarnings("unchecked")
 	protected void setUpAfterContainerInit() throws Throwable {
@@ -30,5 +46,89 @@ public abstract class S2ClickServiceTestCase<T> extends S2ClickTestCase {
 
 		service = (T) SingletonS2Container.getComponent((Class) type);
 	}
+	
+    protected void doRunTest() throws Throwable {
+        TransactionManager tm = null;
+        if (needTransaction()) {
+            try {
+                tm = (TransactionManager) getComponent(TransactionManager.class);
+                tm.begin();
+            } catch (Throwable t) {
+                System.err.println(t);
+            }
+        }
+        try {
+            // TODO ここでキャプチャ
+        	
+            super.doRunTest();
+            
+            // TODO ここでキャプチャ
+            Method method = getTargetMethod();
+            Assert ann = method.getAnnotation(Assert.class);
+            String[] tables = ann.tables();
+            
+        } finally {
+            if (tm != null) {
+                tm.rollback();
+            }
+        }
+    }
+    
+    private void createExcelFile(String[] tables) throws Exception {
+    	JdbcContext context = ((JdbcManagerImplementor) jdbcManager).getJdbcContext();
+    	
+		HSSFWorkbook wb = new HSSFWorkbook();
+		
+    	for(int i=0;i<tables.length;i++){
+    		HSSFSheet sheet = wb.createSheet();
+    		wb.setSheetName(i, tables[i]);
+    		
+	    	PreparedStatement stmt = null;
+	    	ResultSet rs = null;
+	    	try {
+		    	stmt = context.getPreparedStatement("SELECT * FROM " + tables[i]);
+		    	rs = stmt.executeQuery();
+		    	
+		    	ResultSetMetaData md = rs.getMetaData();
+		    	int columnCount = md.getColumnCount();
+		    	HSSFRow row = sheet.createRow(0);
+		    	
+		    	for(int j = 1; j <= columnCount; j++){
+		    		String columnName = md.getColumnName(j);
+		    		HSSFCell cell = row.createCell((short) (j -1));
+		    		cell.setCellValue(new HSSFRichTextString(columnName));
+		    	}
+		    	
+		    	int rowCount = 1;
+		    	while(rs.next()){
+			    	HSSFRow dataRow = sheet.createRow(rowCount);
+			    	for(int j = 1; j <= columnCount; j++){
+			    		HSSFCell cell = dataRow.createCell((short) (j - 1));
+			    		String value = rs.getString(md.getColumnName(j));
+			    		if(value != null){
+			    			cell.setCellValue(new HSSFRichTextString(value));
+			    		}
+			    	}
+			    	rowCount++;
+		    	}
+		    	
+	    	} finally {
+	    		if(rs != null){
+	    			try {
+	    				rs.close();
+	    			} catch(Exception ex){
+	    				;
+	    			}
+	    		}
+	    		if(stmt != null){
+	    			try {
+	    				stmt.close();
+	    			} catch(Exception ex){
+	    				;
+	    			}
+	    		}
+	    	}
+    	}
+    }
 	
 }
